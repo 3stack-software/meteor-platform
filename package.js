4dbb61242e4f5cc49a46c64c3883ef9720ef0fbd
Nathan Muir <ndmuir@gmail.com>
2015-09-21 14:41:39 +1000

    Switch to use `cosmos:browserify`

--- package.js
+++ package.js
@@ -1,12 +1,17 @@
 Package.describe({
   name: '3stack:platform',
-  version: '1.3.0',
+  version: '1.3.0_1',
   summary: 'https://github.com/bestiejs/platform.js',
   git: 'https://github.com/3stack-software/meteor-platform',
   documentation: 'README.md'
 });
 
+Npm.depends({
+  "platform": "1.3.0"
+});
+
 Package.onUse(function(api){
-  api.export('platform', 'client');
-  api.addFiles('platform.js', 'client');
+  api.use('cosmos:browserify@0.4.0');
+  api.export('Platform', 'client');
+  api.addFiles('platform.browserify.js', 'client');
 });
